3245b92e775d861590dd72fce5ecad72017bdc11
ROCKY <51857192+RockyRaut@users.noreply.github.com>
2025-07-09 20:52:23 +0545

    main.py

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ async def vc_kick(ctx, member: discord.Member):
     else:
         await ctx.send("❌ You must be in a VC to kick someone.")
 
-bot.run(os.getenv(DISCORD_TOKEN))
+bot.run(os.getenv("DISCORD_TOKEN"))
